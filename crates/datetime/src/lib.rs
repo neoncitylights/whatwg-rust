65f0a3472b6672bd00961d5ea7012c11172ba350
Samantha Nguyen <contact@samanthanguyen.me>
2024-11-03 23:15:59 -0600

    formatting

--- crates/datetime/src/lib.rs
+++ crates/datetime/src/lib.rs
@@ -1,31 +1,29 @@
-/*!
-A Rust crate for parsing the datetime microsyntax, as defined by the WHATWG HTML Standard.
-
-## Install
-
-```shell
-cargo add whatwg-datetime
-```
-
-## Usage
-
-This library currently implements 8 of the 9 datetime formats defined by the WHATWG HTML Standard. The only format not implemented is the duration format, which is tracked in [issue #23](https://github.com/neoncitylights/whatwg-rust/issues/23).
-
-```rust
-use chrono::{NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Utc};
-use whatwg_datetime::parse_global_datetime;
-
-assert_eq!(
-    parse_global_datetime("2011-11-18T14:54Z"),
-    Some(Utc.from_utc_datetime(
-        &NaiveDateTime::new(
-            NaiveDate::from_ymd_opt(2011, 11, 18).unwrap(),
-            NaiveTime::from_hms_opt(14, 54, 0).unwrap(),
-        )
-    ))
-);
-```
-*/
+//! A Rust crate for parsing the datetime microsyntax, as defined by the WHATWG HTML Standard.
+//!
+//! ## Install
+//!
+//! ```shell
+//! cargo add whatwg-datetime
+//! ```
+//!
+//! ## Usage
+//!
+//! This library currently implements 8 of the 9 datetime formats defined by the WHATWG HTML Standard. The only format not implemented is the duration format, which is tracked in [issue #23](https://github.com/neoncitylights/whatwg-rust/issues/23).
+//!
+//! ```rust
+//! use chrono::{NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Utc};
+//! use whatwg_datetime::parse_global_datetime;
+//!
+//! assert_eq!(
+//!     parse_global_datetime("2011-11-18T14:54Z"),
+//!     Some(Utc.from_utc_datetime(
+//!         &NaiveDateTime::new(
+//!             NaiveDate::from_ymd_opt(2011, 11, 18).unwrap(),
+//!             NaiveTime::from_hms_opt(14, 54, 0).unwrap(),
+//!         )
+//!     ))
+//! );
+//! ```
 
 mod components;
 mod utils;
